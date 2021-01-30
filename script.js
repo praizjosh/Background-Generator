b17ce82dde695fa29bec41ecf13adc040658b069
praizjosh <40269551+praizjosh@users.noreply.github.com>
2021-01-30 12:19:37 +0100

    overall update

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-var  css = document.querySelector("h3");
+var _ = require('lodash');
+
+var body = document.getElementById('gradient'); 
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
-var body = document.getElementById('gradient');
+var css = document.querySelector("h3");
 
-// click, mousecenter, kepress, 
 
 function setGradient() {
 		body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")"
-		
 		css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
